5c8674c00d159d53445689095e7e4ea591a87329
grubeninspekteur <webseiten.designer@googlemail.com>
2016-12-07 14:43:56 +0100

    mention highlighting: doing it the right way...

--- src/better_mention_highlight.js
+++ src/better_mention_highlight.js
@@ -1,23 +1,24 @@
 function handleMention(commentElem) {
-	
-	var mention = $(commentElem).find('.mention');
+
+    var jCommentElem = $(commentElem);
+    var mention = jCommentElem.find('.mention');
 	
     if (mention.length) {
         mention.removeClass('mention');
-		mention.parents('#message').addClass('brbtv-highlight-mention');
+		jCommentElem.addClass('brbtv-highlight-mention');
     }
 }
 
 function include_better_mention_highlight() {
     YouTubeLive.onChatLoaded(function (youtube) {
         addCssToHead(`
-			yt-live-chat-text-message-renderer {
+			yt-live-chat-text-message-renderer.brbtv-highlight-mention {
 				background-color: #00796b;
 				border-top: 0 !important;
 				border-bottom: 0 !important;
 			}
 			
-			yt-live-chat-text-message-renderer #author-name {
+			yt-live-chat-text-message-renderer.brbtv-highlight-mention #author-name {
 			    color: white !important;
 		`);
         youtube.registerChatMessageObserver(handleMention, true);
