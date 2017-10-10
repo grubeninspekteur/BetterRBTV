25a753c2593876f91c1d63a447b29c9838106148
Dominik Schlipper <doom@doom-design.com>
2017-10-10 20:19:45 +0200

    Colored Names Fix (#63)
    Copy Avatar URL via Javascript to the username, so we can color it again with CSS

--- src/colored_names.js
+++ src/colored_names.js
@@ -1,5 +1,18 @@
+function copyAvatarId(commentElem) {
+
+    var jCommentElem = $(commentElem);
+	var avatar = jCommentElem.find('#img');
+	var avatarSrcParts = avatar.attr('src').split('/');
+	var userName = jCommentElem.find('#author-name');
+	
+    if (userName.length && avatar.length) {
+		userName.addClass('brbtv-color-' + avatarSrcParts[3]);
+    }
+}
+
 function include_colored_names() {
     YouTubeLive.onChatLoaded(function (youtube) {
         loadAndAddCssFile('name-colors.css');
+		youtube.registerChatMessageObserver(copyAvatarId, true);
 	});
 }
